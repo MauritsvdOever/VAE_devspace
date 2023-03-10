1c0fba26ea11934826777035a6524359fc742c2f
MauritsvdOever <maurits.vanden.oever@probability.nl>
2023-03-10 15:40:30 +0100

    restructuring

---
+++ __init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 10 15:32:49 2023
+
+@author: MauritsvandenOeverPr
+"""
+
+from Models import VAE
+from Models import MGARCH
+from Data import datafuncs
---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+This repo is created to improve the VAE implementation
+
+Created on Fri Mar 10 15:30:02 2023
+
+@author: MauritsvandenOeverPr
+"""
+
+from Data import datafuncs 
+from Models import MGARCH
+from Models import VAE
